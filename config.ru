809e9b16e27e9517ff3fe1cd3b54bd90dd3670e2
derekviera <ma.dmviera01@gmail.com>
2017-04-28 16:58:55 -0400

    First commit
    Add config.ru
    Add createdb.rb
    Add diabetesapi.rb
    Update .gitignore

---
+++ config.ru
@@ -0,0 +1,3 @@
+require "./diabetesapi"
+
+run Cuba
---
+++ diabetesapi.rb
@@ -0,0 +1,34 @@
+require 'cuba'
+require 'rack/protection'
+require 'json'
+require 'date'
+require 'time'
+require 'sequel'
+# If you need extra protection.
+ Cuba.use Rack::Session::Cookie, :secret => Random.new_seed.to_s
+ Cuba.use Rack::Protection
+ Cuba.use Rack::Protection::RemoteReferrer
+DB = Sequel.connect('sqlite://db/diabetes.sqlite3', :max_connections=>200)
+
+   data = DB[:data]
+# To launch just type: 'rackup' in your console
+Cuba.define do
+  on get do
+
+   on "all" do
+     res.headers["Conent-Type"] = "application/json; charset=utf-8"
+     res.write data.all.to_json
+  end
+
+  on "avg" do 
+    res.headers["Conent-Type"] = "application/json; charset=utf-8"
+    res.write data.avg(:level).to_json
+  end
+
+  on "add/level/*" do
+    # need to look at doesnt take differenet inputs 
+    data.insert(:level => "*")
+    res.write data.where(:level => "*").to_json
+    end
+end
+end
