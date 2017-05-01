9fb340c2da4d4d77cab1d47ebb046690ef2a8b6c
derekviera <ma.dmviera01@gmail.com>
2017-04-30 22:43:53 -0400

    Update diabetesapi.rb
    Have an option to add Date/Time/Blood Sugar Level

--- diabetesapi.rb
+++ diabetesapi.rb
@@ -4,31 +4,35 @@ require 'json'
 require 'date'
 require 'time'
 require 'sequel'
-# If you need extra protection.
- Cuba.use Rack::Session::Cookie, :secret => Random.new_seed.to_s
- Cuba.use Rack::Protection
- Cuba.use Rack::Protection::RemoteReferrer
-DB = Sequel.connect('sqlite://db/diabetes.sqlite3', :max_connections=>200)
 
-   data = DB[:data]
-# To launch just type: 'rackup' in your console
+Cuba.use Rack::Session::Cookie, secret: Random.new_seed.to_s
+Cuba.use Rack::Protection
+Cuba.use Rack::Protection::RemoteReferrer
+DB = Sequel.connect('sqlite://db/diabetes.sqlite3', max_connections: 200)
+
+data = DB[:data]
+
 Cuba.define do
   on get do
-
-   on "all" do
-     res.headers["Conent-Type"] = "application/json; charset=utf-8"
-     res.write data.all.to_json
-  end
-
-  on "avg" do 
-    res.headers["Conent-Type"] = "application/json; charset=utf-8"
-    res.write data.avg(:level).to_json
-  end
-
-  on "level", param("a") do |add|
-    res.headers["Conent-Type"] = "application/json; charset=utf-8"
-    data.insert(:level => "#{add}") 
-    res.write "#{add}".to_json
+    on 'all' do
+      res.headers['Conent-Type'] = 'application/json; charset=utf-8'
+      res.write data.all.to_json
     end
-end
+
+    on 'avg' do
+      res.headers['Conent-Type'] = 'application/json; charset=utf-8'
+      res.write data.avg(:level).to_json
+    end
+
+    # example address localhost:9292/add?d=043017&t=1507&l=120
+    # where ?d= equals the date MMDDYY next to variables must have a & instead
+    # of a ? sign so the time in military time is 15:07 or 3:07pm
+    # then we have the blood sugar level at 120
+
+    on 'add', param('d'), param('t'), param('l') do |d, t, l|
+      res.headers['Conent-Type'] = 'application/json; charset=utf-8'
+      data.insert(date: d.to_s, time: t.to_s, level: l.to_s)
+      res.write data.all.to_json
+    end
+  end
 end
