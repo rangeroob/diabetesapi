dc4a7ac5774f0830f68b2ec2642901cbbb5c620b
derekviera <ma.dmviera01@gmail.com>
2017-05-04 19:12:16 -0400

    update diabetesapi.rb

--- diabetesapi.rb
+++ diabetesapi.rb
@@ -14,14 +14,15 @@ data = DB[:data]
 
 Cuba.define do
   on get do
+    on root do
+      res.redirect '/all'
+    end
+
     on 'all' do
       res.headers['Conent-Type'] = 'application/json; charset=utf-8'
       res.write data.all.to_json
     end
-    on root do
-      res.headers['Conent-Type'] = 'application/json; charset=utf-8'
-      res.redirect 'all'
-    end
+
     on 'avg' do
       res.headers['Conent-Type'] = 'application/json; charset=utf-8'
       res.write data.avg(:level).to_json
