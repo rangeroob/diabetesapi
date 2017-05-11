bcba0a05008256e199b7092c5495fe42971c0dbe
derekviera <ma.dmviera01@gmail.com>
2017-05-11 17:53:09 -0400

    Add edit option to api

--- diabetesapi.rb
+++ diabetesapi.rb
@@ -44,5 +44,11 @@ Cuba.define do
       data.where(date: d.to_s, time: t.to_s, level: l.to_s).delete
       res.write data.all.to_json
     end
+
+    on 'edit', param('id'), param('d'), param('t'), param('l') do |id, d, t, l|
+      res.headers['Content-Type'] = 'application/json; charset=utf-8'
+      data.where(id: id.to_s).update(date: d.to_s, time: t.to_s, level: l.to_s)
+      res.write data.all.to_json
+    end
   end
 end
