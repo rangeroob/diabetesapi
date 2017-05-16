ba16157ada398250a386091881e218b71d14201e
derekviera <ma.dmviera01@gmail.com>
2017-05-15 23:43:53 -0400

    Remove space

--- diabetesapi.rb
+++ diabetesapi.rb
@@ -13,7 +13,6 @@ DB = Sequel.connect('sqlite://db/diabetes.sqlite3', max_connections: 200)
 data = DB[:data]
 
 class AllData < Cuba; end
-
 AllData.define do
   on root do
     res.headers['Content-Type'] = 'application/json; charset=utf-8'
