9bdab747a9a9cf7b7820fe7261ffa6de69d3900f
derekviera <ma.dmviera01@gmail.com>
2017-06-13 00:46:04 -0400

    Add EmailData into module

---
+++ lib/Email_data.rb
@@ -0,0 +1,28 @@
+
+module EmailData
+  DB = Sequel.connect('sqlite://db/diabetes.sqlite3', max_connections: 200)
+  data = DB[:data]
+  class EmailData < Cuba; end
+  EmailData.define do
+    on root, param('address') do |address|
+      res.headers['Content-Type'] = 'text/html; charset=utf-8'
+      options = { address: 'smtp.gmail.com',
+                  port: 587,
+                  domain: 'gmail.com',
+                  user_name: 'test.diabetesapi@gmail.com',
+                  password: 'isdunbfipxjeksph',
+                  authentication: 'plain',
+                  enable_starttls_auto: true }
+      Mail.defaults do
+        delivery_method :smtp, options
+      end
+      Mail.deliver do
+        to address.to_s
+        from 'test.diabetesapi@gmail.com'
+        subject 'Diabetes Data'
+        body data.all.to_json
+      end
+      res.write "email sent to #{address}"
+    end
+  end
+end
