dca4c1ac46c78a955ec5a1aa8f2b76b4c17c1280
derekviera <ma.dmviera01@gmail.com>
2017-07-15 19:07:52 -0400

    Update All_data.rb

--- lib/All_data.rb
+++ lib/All_data.rb
@@ -1,9 +1,9 @@
 
 module AllData
   class AllData < Cuba; end
+  DB = Sequel.connect('sqlite://db/diabetes.sqlite3', max_connections: 200)
+  data = DB[:data]
   AllData.define do
-    DB = Sequel.connect('sqlite://db/diabetes.sqlite3', max_connections: 200)
-    data = DB[:data]
     on root do
       res.headers['Content-Type'] = 'application/json; charset=utf-8'
       res.write data.all.to_json
