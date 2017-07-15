60102ac653f0b3576e7b99df4f138b9d63de5c68
derekviera <ma.dmviera01@gmail.com>
2017-07-15 19:06:11 -0400

    Move code from module to class

--- lib/Add_data.rb
+++ lib/Add_data.rb
@@ -1,8 +1,8 @@
 
 module AddData
+  class AddData < Cuba; end
   DB = Sequel.connect('sqlite://db/diabetes.sqlite3', max_connections: 200)
   data = DB[:data]
-  class AddData < Cuba; end
   AddData.define do
     on root, param('d'), param('t'), param('l') do |d, t, l|
       res.headers['Content-Type'] = 'application/json; charset=utf-8'
