0eba762691668b77952d2a3821c5683ca1c496fe
derekviera <ma.dmviera01@gmail.com>
2017-08-11 11:36:56 -0400

    Update All_data.rb
    Introduce api keys

--- lib/All_data.rb
+++ lib/All_data.rb
@@ -1,12 +1,19 @@
 
 module Api
   class AllData < Cuba; end
-  DB = Sequel.connect('sqlite://db/diabetes.sqlite3', max_connections: 200)
-  data = DB[:data]
+  Sequel.connect('sqlite://db/diabetes.sqlite3', max_connections: 200)
+  data = DB.from(:data)
+  users = DB.from(:users)
   AllData.define do
-    on root do
-      res.headers['Content-Type'] = 'application/json; charset=utf-8'
-      res.write data.all.to_json
+    on root, param('key') do |key|
+      begin
+        authorize_user = JWE.decrypt(key, ENV['key'])
+        res.headers['Content-Type'] = 'application/json; charset=utf-8'
+        res.write data.where(userid: users.select(authorize_user)).all.to_json
+      rescue
+        res.status = 401
+        res.write('Not authorized')
+      end
     end
   end
 end
