3573ab424b77492d7b39e4f356aa2bbe26a86e24
derekviera <ma.dmviera01@gmail.com>
2017-08-11 22:57:51 -0400

    Update Email_data.rb Add key authentication

--- lib/Email_data.rb
+++ lib/Email_data.rb
@@ -1,28 +1,35 @@
 
 module Api
   class EmailData < Cuba; end
-  DB = Sequel.connect('sqlite://db/diabetes.sqlite3', max_connections: 200)
-  data = DB[:data]
+  Sequel.connect('sqlite://db/diabetes.sqlite3', max_connections: 200)
+  data = DB.from(:data)
+  users = DB.from(:users)
   EmailData.define do
-    on root, param('address') do |address|
-      res.headers['Content-Type'] = 'text/html; charset=utf-8'
-      options = { address: 'smtp.gmail.com',
-                  port: 587,
-                  domain: 'gmail.com',
-                  user_name: 'test.diabetesapi@gmail.com',
-                  password: 'isdunbfipxjeksph',
-                  authentication: 'plain',
-                  enable_starttls_auto: true }
-      Mail.defaults do
-        delivery_method :smtp, options
+    on root, param('address'), param('key') do |address, key|
+      begin
+        authorize_user = JWE.decrypt(key, ENV['key'])
+        res.headers['Content-Type'] = 'text/html; charset=utf-8'
+        options = { address: 'smtp.gmail.com',
+                    port: 587,
+                    domain: 'gmail.com',
+                    user_name: 'test.diabetesapi@gmail.com',
+                    password: 'isdunbfipxjeksph',
+                    authentication: 'plain',
+                    enable_starttls_auto: true }
+        Mail.defaults do
+          delivery_method :smtp, options
+        end
+        Mail.deliver do
+          to address.to_s
+          from 'test.diabetesapi@gmail.com'
+          subject 'Diabetes Data'
+          body data.where(userid: users.select(authorize_user)).all.to_json
+        end
+        res.write "email sent to #{address}"
+      rescue
+        res.status = 401
+        res.write('Not authorized')
       end
-      Mail.deliver do
-        to address.to_s
-        from 'test.diabetesapi@gmail.com'
-        subject 'Diabetes Data'
-        body data.all.to_json
-      end
-      res.write "email sent to #{address}"
     end
   end
 end
